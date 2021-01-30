7a815b88d8d7109cc2388f25e77bd08c9b1c7fc0
Joe Rankin <jrankin312@gmail.com>
2021-01-30 14:10:06 -0600

    Initial commit.

---
+++ ScrollViewBug/ScrollViewBug/MainPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ScrollViewBug
+{
+    public class MainPageViewModel
+    {
+        private static Random r = new Random();
+        public ObservableCollection<string> Data { get; set; }
+
+        public MainPageViewModel()
+        {
+            AddDataCommand = new Command(AddData);
+            Data = new ObservableCollection<string>();
+        }
+
+
+        public ICommand AddDataCommand { get; set; }
+
+        public void AddData()
+        {
+            Data.Add(r.Next(1000, 10000).ToString());
+        }
+    }
+}
